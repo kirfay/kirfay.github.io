e15bd821ce145c82bb987aa67f2fe11ca8800a41
kirfay <168469895+kirfay@users.noreply.github.com>
2024-08-04 05:39:37 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+// Header Toggle 
+let MenuBtn = document.getElementById('MenuBtn')
+
+MenuBtn.addEventListener('click', function(e) {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+})
